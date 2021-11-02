fdbfa75fe0a608388c97c92533db0e28a992ed8b
Ivette Marie <thetechnologygal@gmail.com>
2021-11-02 11:23:51 -0400

    Updated-final-project

---
+++ js/index.js
@@ -0,0 +1,12 @@
+console.log("connected!")
+
+
+class TaskManager {
+    constructor() {
+      this.tasks = {
+      };
+    console.log("tasks")
+    }
+  
+  }
+    
---
+++ js/taskManager.js
@@ -0,0 +1,12 @@
+
+
+
+class TaskManager {
+    constructor() {
+      this.tasks = {
+      };
+    console.log("tasks")
+    }
+  
+  }
+    
