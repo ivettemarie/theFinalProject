dbe23032ef0e6fde16357b0e64d8ee540af0e1c5
Ivette Marie <thetechnologygal@gmail.com>
2021-11-03 08:34:55 -0400

    updating-tasks-now-task7

--- js/index.js
+++ js/index.js
@@ -1,7 +1,87 @@
 console.log("connected!")
 
+// ============================================//
 
 
-let taskManager = new TaskManager()
+//Initializes the taskManager object
+let taskManager = new TaskManager();
+
+//Gets the task form element
+let taskForm = document.querySelector("#taskForm");
+
+//Function that validates input, and sets error messages
+function validFormFieldInput() {
+    let isValid = true;
+    let taskName = document.querySelector("input[name=name]").value;
+    let errorMessage = "";
+    if (taskName == "") {
+        errorMessage += "Task name cannot be empty <br/>";
+        isValid = false;
+    }
+    let taskDescription = document.querySelector("input[name=description]").value;
+    if (taskDescription == "") {
+        errorMessage += "Task description cannot be empty <br/>";
+        isValid = false;
+    }
+    let taskStatus = document.querySelector("#status").value;
+    if (taskStatus == "") {
+        errorMessage += "Task status cannot be empty <br/>";
+        isValid = false;
+    }
+    let taskAssigned = document.querySelector("input[name=assignedTo").value;
+    if (taskAssigned == "") {
+        errorMessage += "Task assigned to cannot be empty <br/>";
+        isValid = false;
+    }
+    let taskDate = document.querySelector("#tasktime").value;
+    if (taskDate == "") {
+        errorMessage += "Task date cannot be empty <br/>";
+        isValid = false;
+    }
+    let alert = document.querySelector("#alert");
+    alert.innerHTML = errorMessage;
+    return isValid;
+}
+
+//Sets a submit listener to task form, checks if valid to submit, or show error messages if not
+taskForm.addEventListener("submit", function(e){
+    e.preventDefault();
+
+    //Checks if form is valid
+    let isValid = validFormFieldInput();
+
+    //Gets alert and success boxes (on top of form)
+    let alert = document.querySelector("#alert");
+    let success = document.querySelector("#success");
+    if (isValid == false) {
+        alert.classList.remove("d-none");
+        success.classList.add("d-none");
+        //Return and does not execute anything after this line.
+        return;
+    } else {
+        success.classList.remove("d-none");
+        alert.classList.add("d-none");
+    }
+
+    //Gets all form values
+    let taskName = document.querySelector("input[name=name]").value;
+    let taskDescription = document.querySelector("input[name=description]").value;
+    let taskStatus = document.querySelector("#status").value;
+    let taskAssigned = document.querySelector("input[name=assignedTo").value;
+    let taskDate = document.querySelector("#tasktime").value;
+
+    //Adds new task to task manager array
+    taskManager.addTask(taskName, taskDescription, taskAssigned, taskDate, taskStatus);
+    
+    //Resets form
+    taskForm.reset();
+
+    //Re-renders card list with new tasks
+    taskManager.render();
+
+}) 
+
+
+
 
 
--- js/taskManager.js
+++ js/taskManager.js
@@ -1,12 +1,71 @@
 console.log("connected!")
 
 
+const createTaskHtml = (name, description, assignedTo, dueDate, status) =>{ 
+    let cardHtml = `<div class="card mx-auto" style="width: 18rem;">
+                                <div class="card-body">
+                                <h4 class="taskName">${name}</h4>
+                                <h6 class="taskDescription">${description}</h6>
+                                <h6 class="taskAssignedTo">${assignedTo}</h6>
+                                <h6 class="taskDueDate">${dueDate}</h6>
+                                <span class="taskStatus">${status}</span>
+                                <a href="#" class="btn btn-primary">Delete</a>
+                                </div>
+                            </div>`;
+
+    return cardHtml;
+}
+
 class TaskManager {
-    constructor() {
+    //Constructor - Runs when using new
+    constructor(currentId = 0) {
       this.tasks = [];
-      
-    console.log(this.tasks)
+      this.currentId = currentId;
+
+       console.log(this.tasks)
+    }
+
+    //Method, adds a new task to this.tasks array
+    addTask( name, description, assignedTo, dueDate, status = "Todo"){
+        this.currentId += 1;
+        let task = {
+            id: this.currentId,
+            name: name,
+            description: description,
+            assignedTo: assignedTo,
+            dueDate: dueDate,
+            status: status
+        }
+        console.log(task,this.tasks)
+        this.tasks.push(task);
+    }
+
+    //Method, renders a card list using the tasks in the this.tasks array
+    render(){
+        let tasksHtmlList = [];
+
+        //Loops around the tasks array
+        for(let i = 0; i < this.tasks.length; i++){
+            let task = this.tasks[i];
+            //Gets date and formats it to readable format
+            let date = new Date(task.dueDate);
+            let formattedDate = date.toDateString();
+            let taskHtml = createTaskHtml(task.name, task.description, task.assignedTo, formattedDate, task.status);
+            //Adds task to html array
+            tasksHtmlList.push(taskHtml);
+        }
+
+        //Joins array to single html string
+        const tasksHtml = tasksHtmlList.join("\n");
+
+        //Adds string to main html
+        document.querySelector("#list-group").innerHTML = tasksHtml;
+
     }
   
   }
-    
+
+
+
+
+  
